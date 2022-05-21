eb1a9bf61cc6166a784f789b0bb6854e430689a2
c4m3lion <86702015+c4m3lion@users.noreply.github.com>
2022-05-21 14:22:20 +0300

    daily uipdate

---
+++ lib/screens/Phone/VideoPage/phone_video.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhoneVideo extends StatefulWidget {
+  const PhoneVideo({Key? key}) : super(key: key);
+
+  @override
+  State<PhoneVideo> createState() => _PhoneVideoState();
+}
+
+class _PhoneVideoState extends State<PhoneVideo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
